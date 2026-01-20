4a82f302c051b23a3a3abe5eb57d12be93dc8023
Nur Alam <nuralam1122345@gmail.com>
2026-01-20 23:25:04 +0600

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-
 import "./App.css";
+import Countries from "./Components/Countries/Countries";
+
+const countriesPromise = fetch(
+  "https://restcountries.com/v3.1/all?fields=name,flags,population,languages,capital",
+).then((res) => res.json());
+console.log(countriesPromise)
 
 function App() {
-  
-
-  return <>
-  <div>
-<h1>
-  rest country
-</h1>
-  </div>
-  </>;
+  return (
+    <>
+      <div>
+        <Countries></Countries>
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/Components/Countries/Countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            hello bangladesh
+        </div>
+    );
+};
+
+export default Countries;
